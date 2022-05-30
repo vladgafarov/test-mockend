63bc6d5d55bb38b77ec1e5f44c9e2401bbf3995c
Vlad <vlad.gafarov02@gmail.com>
2022-05-30 16:58:23 +0500

    Add getPosts

---
+++ app/models/post.server.ts
@@ -0,0 +1,16 @@
+import type { Post } from "~/interfaces/Post"
+
+interface IGetPosts {
+	limit?: number
+	offset?: number
+}
+
+export async function getPosts({
+	limit = 100,
+	offset = 0,
+}: IGetPosts): Promise<Post[]> {
+	const res = await fetch(
+		`${process.env.BACKEND_URL}/posts?limit=${limit}&offset=${offset}`,
+	)
+	return await res.json()
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,36 +1,24 @@
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import PostCard from "~/components/PostCard"
+import type { Post } from "~/interfaces/Post"
+import { getPosts } from "~/models/post.server"
+
+export async function loader() {
+	const posts = await getPosts({
+		limit: 3,
+	})
+	return json(posts)
+}
+
 export default function Index() {
+	const posts = useLoaderData() as Post[]
+
 	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-			<h1>Welcome to Remix</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/docs"
-						rel="noreferrer"
-					>
-						Remix Docs
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+		<main>
+			{posts.map((post) => (
+				<PostCard key={post.id} post={post} />
+			))}
+		</main>
+	)
 }
